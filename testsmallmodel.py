332931a8257016bcb9d6a16ed6c9e7e7d5c63b25
Claire Jensen <clairej735@gmail.com>
2026-09-23 13:57:17 -0700

    replace feature cols var

--- testsmallmodel.py
+++ testsmallmodel.py
@@ -235,13 +235,13 @@ def main():
     # fs.put(model_path, f"{S3_BUCKET}/cjense/data/testmodel/{GLACIER_NAME}_xgb_{RESOLUTION}.json")
     # print(f"\nModel saved to S3.")
     
-    feature_cols = model.get_booster().feature_names
+    # feature_cols = model.get_booster().feature_names
 
-    masker = shap.maskers.Independent(train[feature_cols], max_samples=len(train))
+    masker = shap.maskers.Independent(train[FEATURE_COLS], max_samples=len(train))
     explainer = shap.TreeExplainer(model, masker)
-    shap_vals = explainer(test[feature_cols])
+    shap_vals = explainer(test[FEATURE_COLS])
     
-    shap.plots.beeswarm(shap_vals[feature_cols], show=False)
+    shap.plots.beeswarm(shap_vals[FEATURE_COLS], show=False)
     plt.savefig('figures/beeswarm.png', dpi=300)
     
     return model, metrics
